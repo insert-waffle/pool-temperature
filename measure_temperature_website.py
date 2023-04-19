0c5d6040b647f8faf9879c45dc59b96aaa3ab28f
Jens Maes <mail@ditisjens.be>
2023-04-19 15:18:59 +0200

    trying out if I can make this a static page on a webserver.

---
+++ measure_temperature_website.py
@@ -0,0 +1,47 @@
+import os
+import glob
+import time
+import sys
+import datetime
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    iso = time.ctime()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+    return temp_c
+
+read_temp()
+
+poolTemp = read_temp()
+
+file_html = open("demo.html", "w")
+
+file_html.write('''<html>
+<head>
+<title>Zwembad</title>
+</head> 
+<body>
+<h1>{poolTemp}</h1>           
+</body>
+</html>''')
+
+file_html.close()
